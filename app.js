c4be3227ad30858694b24559525b7c97b2c41a37
varunmulinti <varun9.run@gmail.com>
2016-06-09 22:16:51 +0530

    starting with third task

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ angularApp.config(function ($routeProvider,$httpProvider){
     controller: 'HomeController',
     controllerAs: 'hc'
   })
+  .when('/',{
+    templateUrl:'pages/second.html',
+    controller:'SecondController',
+    controllerAs:'sc'
+  })
 });
-angularApp.controller("HomeController",['$resource','$routeParams',function($resource,$routeParams){
+angularApp.controller("HomeController",['$resource',function($resource){
   var vm = this;
-
-
     vm.getdetails = function (){
     var details =$resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP',
    {FIPS:vm.country,time:vm.year,key:'4d7e81b99a7305297e1ed81f1f9c923a17010890'});
@@ -19,8 +22,14 @@ angularApp.controller("HomeController",['$resource','$routeParams',function($res
     return vm.response;
 
   }
-
 }]);
+angularApp.controller("SecondController",['$resource',function ($resource){
+  var vm = this;
+  vm.getdetails =function(){
+
+  }
+
+}])
 
 
 
