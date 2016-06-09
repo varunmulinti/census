38b55e0751eb6476a428dd36b6b19d03f709680e
varunmulinti <varun9.run@gmail.com>
2016-06-09 21:58:54 +0530

    done with landing page

--- app.js
+++ app.js
@@ -7,31 +7,33 @@ angularApp.config(function ($routeProvider,$httpProvider){
     controllerAs: 'hc'
   })
 });
-angularApp.controller("HomeController",['$resource','$routeParams','landingservice',function($resource,$routeParams,landingservice){
+angularApp.controller("HomeController",['$resource','$routeParams',function($resource,$routeParams){
   var vm = this;
 
 
-  vm.getdetails = function (){
+    vm.getdetails = function (){
     var details =$resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP',
    {FIPS:vm.country,time:vm.year,key:'4d7e81b99a7305297e1ed81f1f9c923a17010890'});
     vm.response = details.query();
-    return vm.response;
     console.log(vm.response);
+    return vm.response;
+
   }
 
 }]);
 
 
 
-angularApp.service('landingservice',function($resource,$routeParams){
-  var vm = this;
-  vm.country = "";
-  vm.year = "";
-  vm.getdetails = function (){
-    var details =$resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP',
-   {FIPS:vm.country,time:vm.year,key:'4d7e81b99a7305297e1ed81f1f9c923a17010890'});
-    vm.response = details.query();
-    return vm.response;
-    console.log(vm.response);
-  }
-});
+
+// angularApp.service('landingservice',function($resource,$routeParams){
+//   var vm = this;
+  // vm.country = "";
+  // vm.year = "";
+  // vm.getdetails = function (){
+  //   var details =$resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP',
+  //  {FIPS:vm.country,time:vm.year,key:'4d7e81b99a7305297e1ed81f1f9c923a17010890'});
+  //   vm.response = details.query();
+  //   return vm.response;
+  //   console.log(vm.response);
+  // }
+// });
